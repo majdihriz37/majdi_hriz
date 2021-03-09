6daf2527697cd8202e8e61d3a8936045b961f99d
majdi <majdihriz@gmail.com>
2021-03-09 21:13:03 +0100

    just setting up my account

---
+++ index.php
@@ -0,0 +1,29 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Majdi Hriz</title>
+</head>
+
+<body>
+    <?php 
+    $frameworks=array('CakePhp', 'Symfony', 'ZendFramework', 'CodeIgniter', 'Laravel');
+    $frameworksArray=null;$count=0;$booksArray=null;
+    foreach ($frameworks as $frameworks){
+        $frameworksArray .= $count>0?' , ' . $frameworks:$frameworks;
+        $count+=1;
+    }
+    $books=array('The Vanishing Half'=>'Brit Bennett','American Dirt'=>' Jeanine Cummins');
+    foreach($books as $key => $value) {
+        $booksArray.= "<a id='fett' style='font-weight:bold'>" . $key . "</a>  : " . $value.'<br>';
+    }
+    ?>
+    
+    <?='<h2>das ist eine <strong>HTML-Boilerplate</strong></h2>'?>
+    <?='<h1>Aufgabe 22 :<br> '.$frameworksArray.'</h1>'?>
+    <?='<h1>Aufgabe 25 :<br> '.$booksArray.'</h1>'?>
+</body>
+</html>
